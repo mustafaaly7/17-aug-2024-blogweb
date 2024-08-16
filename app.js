9d82b894e1f9aa0f38fe0f0d22015e687f0dbd79
Mustafa Ali <mustafaaaly27@gmail.com>
2024-08-17 02:38:10 +0500

    not completed

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import {
 ,setDoc
 
 
+,ref,
+uploadBytesResumable,
+getDownloadURL,storage
 
 
   } from "../firebase.js"
@@ -16,12 +19,125 @@ window.location.replace("./pages/login.html")
 
 }
  
-const postBlog = async() =>{
+const titleBlog = document.getElementById("titleBlog")
+const contentBlog = document.getElementById("contentBlog")
+const blogimg = document.getElementById("blogimg")
+const parent = document.querySelector(".parent")
+const uid = localStorage.getItem("userId")
 
+const flexSwitchCheckChecked = document.getElementById("flexSwitchCheckChecked")
+
+
+
+// image upload function 
+const imgUpload = (file) =>{
+  return new Promise((resolve, reject) => {
+    // Create the file metadata
+  /** @type {any} */
+  const metadata = {
+    contentType: 'image/jpeg'
+  };
+  
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  const storageRef = ref(storage, 'images/' + file.name);
+  const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+  
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on('state_changed',
+    (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    }, 
+    (error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      // switch (error.code) {
+      //   case 'storage/unauthorized':
+      //     // User doesn't have permission to access the object
+      //     break;
+      //   case 'storage/canceled':
+      //     // User canceled the upload
+      //     break;
+  
+      //   // ...
+  
+      //   case 'storage/unknown':
+      //     // Unknown error occurred, inspect error.serverResponse
+      //     break;
+      // }
+      reject(error)
+    }, 
+    () => {
+      // Upload completed successfully, now we can get the download URL
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('File available at', downloadURL);
+      });
+    }
+  );
+  
+  })
+  
+  
+    
+  }
+
+
+const postBlog = async() =>{
+try {
+  
+// console.log(blogimg.files[0]);
+// console.log(titleBlog.value);
+// console.log(contentBlog.value);
+// console.log(flexSwitchCheckChecked.checked);
+
+const imgUrl = await imgUpload(blogimg.files[0])
+
+const blogObj = {
+  title : titleBlog.value,
+  content :contentBlog.value ,
+  image :imgUrl,
+  privacy : flexSwitchCheckChecked.checked,
+  uid : uid
+}
+console.log( imgUrl, blogObj);
+ await addDoc(collection(db, "blogs"), blogObj);
+// console.log(blogUpload);
+
+// var myModalEl = document.getElementById("exampleModal");
+// var modal = bootstrap.Modal.getInstance(myModalEl);
+// modal.hide();
+
+// alert("Blog Sucessfully Created.");
+
+
+  
+} catch (error) {
+  alert(error.message)
+}
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 const signOut = () =>{
--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
   import { getFirestore,collection, addDoc,getDocs ,  doc, setDoc ,  getDoc} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
   import { getAuth , createUserWithEmailAndPassword,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-  import { getStorage,ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+  import { getStorage,ref, uploadBytesResumable, getDownloadURL  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
